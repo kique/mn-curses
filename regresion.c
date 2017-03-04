851c750a3ef9a19ca1aed5fe2c886ca7aa8fca9a
Enrique M. Roldan <enrique.roldan@gmail.com>
2017-03-03 18:11:20 -0600

    Algo se modifico

---
+++ regresion.c
@@ -0,0 +1,15 @@
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  regresion_poli
+ *  Description:  
+ * =====================================================================================
+ */
+	void
+regresion_poli ( int m, int n, double *apX, double *apY )
+{
+	
+	if ( n <= m+1 ) {
+		printf("\nLa regresión no es posible
+	}
+}		/* -----  end of function regresion_poli  ----- */
